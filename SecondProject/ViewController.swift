91e9f206e8845a889ecb3653a0b184b58f8029de
Tatsiana <tanya3308225@mail.ru>
2022-02-08 21:22:35 +0300

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by Tatsiana Marchanka on 8.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
